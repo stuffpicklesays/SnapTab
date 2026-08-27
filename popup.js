30f5718deeb19376ec2b1f77f1c1f8a217f736ad
Nathaniel Rogers <pikapikapickle2@gmail.com>
2026-08-27 20:39:41 +0100

    added settings skeleton

--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
+document.getElementById('settings-btn').addEventListener('click', () => {
+    const settingsDiv = document.getElementById('settings');
+    settingsDiv.style.display = settingsDiv.style.display === 'none' ? 'block' : 'none';
+});
 chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
     document.getElementById('url').innerText = tab.url;
     new QRCode(document.getElementById("qrcode"), { text: tab.url, width: 160, height: 160 });
